02f165ed68df1929e825e67bf98bd6d2016ae188
yoshikazu kishida <k.yoshikazu1221@gmail.com>
2021-03-25 18:11:57 +0900

    NEW: add CRUD

--- dist/controllers/todos.js
+++ dist/controllers/todos.js
@@ -1,12 +1,36 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createTodo = void 0;
+exports.deleteTodo = exports.updateTodo = exports.getTodos = exports.createTodo = void 0;
 var todo_1 = require("../models/todo");
 var TODOS = [];
 var createTodo = function (req, res, next) {
     var text = req.body.text;
     var newTodo = new todo_1.Todo(Math.random().toString(), text);
     TODOS.push(newTodo);
-    res.status(201).json({ message: 'TODOを作成しました。', createdTodo: newTodo });
+    res.status(201).json({ message: 'TODO', createdTodo: newTodo });
 };
 exports.createTodo = createTodo;
+var getTodos = function (rew, res, next) {
+    res.json({ todos: TODOS });
+};
+exports.getTodos = getTodos;
+var updateTodo = function (req, res, next) {
+    var todoId = req.params.id;
+    var updateText = req.body.text;
+    var todoIndex = TODOS.findIndex(function (todo) { return todo.id === todoId; });
+    if (todoIndex < 0) {
+        throw new Error("Error");
+    }
+    TODOS[todoIndex] = new todo_1.Todo(todoId, updateText);
+};
+exports.updateTodo = updateTodo;
+var deleteTodo = function (req, res, next) {
+    var todoId = req.params.id;
+    var todoIndex = TODOS.findIndex(function (todo) { return todo.id === todoId; });
+    if (todoIndex < 0) {
+        throw new Error("Error");
+    }
+    TODOS.splice(todoIndex);
+    res.json({ message: "DELETE" });
+};
+exports.deleteTodo = deleteTodo;
--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -10,5 +10,36 @@ export const createTodo: RequestHandler = (req, res, next) => {
 
   TODOS.push(newTodo);
 
-  res.status(201).json({message: 'TODOを作成しました。', createdTodo: newTodo});
+  res.status(201).json({message: 'TODO', createdTodo: newTodo});
 }
+
+export const getTodos: RequestHandler = (rew,res, next) => {
+    res.json({todos: TODOS});
+}
+
+export const updateTodo: RequestHandler<{id: string}> = (req, res, next) => {
+    const todoId = req.params.id
+    const updateText = (req.body as { text: string }).text;
+
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+    if (todoIndex < 0) {
+        throw new Error("Error");
+    }
+
+    TODOS[todoIndex] = new Todo(todoId, updateText);
+}
+
+export const deleteTodo: RequestHandler = (req, res, next) => {
+    const todoId = req.params.id
+
+    const todoIndex = TODOS.findIndex(todo => todo.id === todoId);
+
+    if (todoIndex < 0) {
+        throw new Error("Error");
+    }
+
+    TODOS.splice(todoIndex);
+    
+    res.json({message: "DELETE"});
+  }
